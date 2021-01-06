b732250d50ae6e8085cd2156b3f353318eeca3bf
wassimbj <wassimbenjdida@gmail.com>
2021-01-06 14:05:38 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,25 @@
+> a simple pure go web api
+
+first run the file (wich is the server)
+```bash
+go run main.go
+```
+
+there is two routes there
+/products => wich is a GET route, that will return the products
+/create => wich is a POST route, it will create a product; the data is stored in memory no DB is used.
+
+open the cmd and write
+```bash
+curl http://localhost:1234/products
+```
+to get the products (its a slice of structs)
+
+and
+```bash
+curl -X POST --data "{ \"name\": \"Iphone\", \"price\": 999}" http://localhost:1234/create
+```
+to create a products, it just append the data to the slice.
+
+
+this is just for learning purpose.
---
+++ main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strings"
+
+	"github.com/rs/cors"
+)
+
+type Product struct {
+	Name  string `json:"name"`
+	Price int    `json:"price"`
+}
+
+var products []Product
+
+func getProducts(res http.ResponseWriter, req *http.Request) {
+	reqMethod := req.Method
+	if reqMethod != "" && strings.ToLower(reqMethod) != "get" {
+		res.WriteHeader(400)
+		res.Write([]byte("Invalid Method, expected GET"))
+		return
+	}
+	res.Header().Set("Content-Type", "application/json")
+	jsonP, _ := json.Marshal(products)
+	res.Write(jsonP)
+}
+
+func createProduct(res http.ResponseWriter, req *http.Request) {
+	reqMethod := req.Method
+	if reqMethod == "" || strings.ToLower(reqMethod) != "post" {
+		res.WriteHeader(400)
+		res.Write([]byte("Invalid Method, expected POST"))
+		return
+	}
+
+	body, _ := ioutil.ReadAll(req.Body)
+
+	var data Product
+	json.Unmarshal(body, &data)
+	products = append(products, data)
+	fmt.Println("Added product: ", products)
+	res.WriteHeader(200)
+	res.Write([]byte("Product created successfully"))
+}
+
+func main() {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/products", getProducts) // GET
+
+	mux.HandleFunc("/create", createProduct) // GET
+
+	// init server
+	handler := cors.Default().Handler(mux)
+	err := http.ListenAndServe(":1234", handler)
+
+	if err != nil {
+		panic("Server is DOWN !!!")
+	}
+
+	fmt.Println("Server is running...")
+}
